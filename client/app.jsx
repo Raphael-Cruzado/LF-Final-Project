792baa9a15fea7db4f13bdc0efb2c6fd2af4611c
Lance Cruzado <raphaellcruzado@gmail.com>
2022-05-31 19:55:43 +0000

    parseRoute setup

--- client/app.jsx
+++ client/app.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
 import Home from './pages/home';
+import { parseRoute } from './lib';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', e => {
+      e.preventDefault();
+      this.setState({ route: parseRoute(window.location.hash) });
+    });
+  }
+
+  // renderPage() {
+  //   const { route } = this.state;
+  //   if (route.path === '') {
+  //     return <Catalog />;
+  //   }
+  //   if (route.path === 'products') {
+  //     const productId = route.params.get('productId');
+  //     return <ProductDetails productId={productId} />;
+  //   }
+  //   return <NotFound />;
+  // }
+
   render() {
     return <Home />;
   }
