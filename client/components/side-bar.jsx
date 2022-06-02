22afc9bb38265b7c349cd9d48776ba978ca56aa8
Lance Cruzado <raphaellcruzado@gmail.com>
2022-06-02 22:13:47 +0000

    fixed sidebar

--- client/components/side-bar.jsx
+++ client/components/side-bar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   CDBSidebar,
   CDBSidebarContent,
@@ -7,29 +7,38 @@ import {
   CDBSidebarMenu,
   CDBSidebarMenuItem
 } from 'cdbreact';
-import { NavLink } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import * as FaIcons from 'react-icons/fa';
 
 export default function SideBar() {
+  const [show, setShow] = useState('');
+  const [active, setActive] = useState(true);
+
   return (
     <div
       style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}
     >
       <CDBSidebar textColor="#fff" backgroundColor="#B7D7BF">
-        <CDBSidebarHeader prefix={<FaIcons.FaBars />}>
+        <CDBSidebarHeader
+        prefix={<FaIcons.FaBars onClick={() => {
+          active === true ? setActive(false) : setActive(true);
+          active === true ? setShow('CLASSES') : setShow('');
+        }}/>}>
           <a
             href="/"
             className="text-decoration-none"
             style={{ color: 'inherit' }}
           >
-            username
+            Flash Cards
           </a>
         </CDBSidebarHeader>
         <CDBSidebarContent >
           <CDBSidebarMenu prefix={<FaIcons.FaRegCalendarPlus />}>
-            <h4 id='class-header'>CLASSES <FaIcons.FaRegCalendarPlus /></h4>
+            <h4
+            id='class-header'>{show}
+            <FaIcons.FaRegCalendarPlus />
+            </h4>
             <CDBSidebarMenuItem icon='sticky-note'>Dummy Class</CDBSidebarMenuItem>
             <CDBSidebarMenuItem icon='sticky-note'>Dummy Class</CDBSidebarMenuItem>
           </CDBSidebarMenu>
