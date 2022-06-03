bb8665655513b7b2a96760fe9b1a38570629fd40
Lance Cruzado <raphaellcruzado@gmail.com>
2022-06-03 06:53:18 +0000

    home header done

--- client/components/home-header.jsx
+++ client/components/home-header.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import {
+  CDBContainer,
   CDBBox,
-  CDBContainer
+  CDBBtn
 } from 'cdbreact';
+import { Row, Col } from 'react-bootstrap/';
+import * as FaIcons from 'react-icons/fa';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function HomeHeader() {
   return (
     <CDBContainer>
-      <CDBBox display='flex' justifyContent='center'>
-        Hello World
+      <CDBBox id='home-header' display='flex' justifyContent='start'>
+        <Row className='header-img'>
+            <Col>
+              <img src="./img/unisex-avatar.svg" alt="avatar" id='avatar'/>
+            </Col>
+          <Col>
+            <Col id='header-name'>
+              <h1>Class Name </h1>
+              <span id='pen'><FaIcons.FaPen size={30} /></span>
+            </Col>
+            <CDBBtn id='study-btn' circle>
+              <FaIcons.FaPlay />
+              STUDY
+            </CDBBtn>
+          </Col>
+        </Row>
       </CDBBox>
+      <hr />
     </CDBContainer>
   );
 }
