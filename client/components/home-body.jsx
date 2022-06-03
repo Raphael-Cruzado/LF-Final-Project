3cdb4b507541a6b096afe1ae6c8efdec373818f4
Lance Cruzado <raphaellcruzado@gmail.com>
2022-06-03 18:50:27 +0000

    feature-1 homepage body

--- client/components/home-body.jsx
+++ client/components/home-body.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
-  CDBBox
+  CDBBox,
+  CDBBtn
 } from 'cdbreact';
 import { Row, Col } from 'react-bootstrap/';
 import * as FaIcons from 'react-icons/fa';
@@ -12,16 +13,27 @@ export default function HomeBody() {
       <CDBBox display='flex' justufy-content='flex-start' id='deck-wrapper'>
         <ul id='deck-list'>
           <Row className='deck-row'>
-            <li>
-              <Col><FaIcons.FaBookmark size={39} /></Col>
-              <Col><h3>Dummy Deck</h3></Col>
+            <li className='flex-row space-content'>
+              <Col className='flex-row'>
+                <FaIcons.FaBookmark size={39} />
+                <h3 className='deck-title'>Dummy Deck</h3>
+              </Col>
+              <Col className='flex-end'>
+                <CDBBtn id='study-deck' circle>
+                  <FaIcons.FaPlay />
+                </CDBBtn>
+                <FaIcons.FaPen size={45} className='center-deck-item' />
+                <FaIcons.FaTrashAlt size={45} className='center-deck-item'/>
+              </Col>
             </li>
           </Row>
         </ul>
         <hr />
         <Row className='deck-row'>
-          <Col><FaIcons.FaPlusSquare size={50}/></Col>
-          <Col><h2>Create New Deck</h2></Col>
+          <Col className='flex-row'>
+            <FaIcons.FaPlusSquare size={50} />
+            <h2 className='deck-title'>Create New Deck</h2>
+          </Col>
         </Row>
       </CDBBox>
     </>
