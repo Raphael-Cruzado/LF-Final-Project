8c41c2e173925677cff44d23e56fd923b2ab0059
Lance Cruzado <raphaellcruzado@gmail.com>
2022-06-09 22:34:53 +0000

    finished appending deck & classes through the modal

--- client/components/home-header.jsx
+++ client/components/home-header.jsx
@@ -18,7 +18,14 @@ export default function HomeHeader() {
           <Col>
             <Col id='header-name'>
               <h1>Class Name </h1>
-              <span id='pen'><FaIcons.FaPen size={30} /></span>
+              <span id='pen'>
+                <FaIcons.FaPen size={30} style={{ position: 'relative', left: '7px' }} />
+                <FaIcons.FaTrashAlt
+                cursor='pointer' size={36}
+                className='center-deck-item'
+                style={{ position: 'relative', bottom: '35px', left: '19px' }}
+                />
+                </span>
             </Col>
             <CDBBtn id='study-btn' circle>
               <FaIcons.FaPlay />
