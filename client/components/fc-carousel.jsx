88187119676cb2a7385fd292c3225d345cef60ca
Lance Cruzado <raphaellcruzado@gmail.com>
2022-07-05 23:18:47 +0000

    fc-carousel

--- client/components/fc-carousel.jsx
+++ client/components/fc-carousel.jsx
@@ -8,7 +8,38 @@ import {
 } from 'cdbreact';
 
 function flashCardCarousel() {
-
+  return (
+    <CDBContainer>
+      <CDBContainer>
+        <CDBCarousel
+          activeItem={1}
+          length={3}
+          showControls={true}
+          showIndicators={true}
+          className="z-depth-1"
+          slide
+        >
+          <CDBCarouselInner>
+            <CDBCarouselItem itemId="1">
+              <CDBView>
+                <img className="d-block w-100" src="https://imgur.com/oPU7JbX" alt="First slide" />
+              </CDBView>
+            </CDBCarouselItem>
+            <CDBCarouselItem itemId="2">
+              <CDBView>
+                <img className="d-block w-100" src="https://imgur.com/EgVSItY" alt="Second slide" />
+              </CDBView>
+            </CDBCarouselItem>
+            <CDBCarouselItem itemId="3">
+              <CDBView>
+                <img className="d-block w-100" src="https://imgur.com/c1aFo29" alt="Third slide" />
+              </CDBView>
+            </CDBCarouselItem>
+          </CDBCarouselInner>
+        </CDBCarousel>
+      </CDBContainer>
+    </CDBContainer>
+  );
 }
 
 export default flashCardCarousel;
