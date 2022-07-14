9b2655a0531b744a3d5d2f047157255f4312e9cf
Lance Cruzado <raphaellcruzado@gmail.com>
2022-07-14 23:53:35 +0000

    add flashcard

--- client/components/fc-form.jsx
+++ client/components/fc-form.jsx
@@ -17,11 +17,13 @@ function FcForm({ cardData }) {
             <CDBBtn id='study-card' style={{ position: 'relative', left: '2.5rem' }} circle>
               <h4>Study Deck</h4>
             </CDBBtn>
-            <CDBInput type='text' placeholder='Front Content' style={{ width: '40rem' }} />
-            <CDBInput type='text' placeholder='Back Content' style={{ width: '40rem' }} />
-            <CDBBtn id='study-card' style={{ position: 'relative', top: '1rem', left: '2.5rem' }} circle>
-              <h4>Create Card</h4>
-            </CDBBtn>
+            <form>
+              <CDBInput type='text' placeholder='Front Content' style={{ width: '40rem' }} />
+              <CDBInput type='text' placeholder='Back Content' style={{ width: '40rem' }} />
+              <CDBBtn id='study-card' style={{ position: 'relative', top: '.5rem', left: '2.5rem' }} circle>
+                <h4>Create Card</h4>
+              </CDBBtn>
+            </form>
           </Col>
         </Col>
       </>
