096ef36f33abdaead73a6690d80fec7a2f08d628
Lance Cruzado <raphaellcruzado@gmail.com>
2022-07-14 00:33:05 +0000

    fc-carousel

--- client/components/fc-carousel.jsx
+++ client/components/fc-carousel.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import '../node_modules/bootstrap';
 
 export default function FlashCardCarousel() {
   return (
-    <div className="card" style={{ width: '18rem' }}>
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card&apos;s content.</p>
-        <a href="#" className="card-link">Card link</a>
-        <a href="#" className="card-link">Another link</a>
-      </div>
-    </div>
+    <h1>hello world</h1>
   );
 }
+
+// <div className="card" style={{ width: '18rem' }}>
+//   <div className="card-body">
+//     <h5 className="card-title">Card title</h5>
+//     <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+//     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card&apos;s content.</p>
+//     <a href="#" className="card-link">Card link</a>
+//     <a href="#" className="card-link">Another link</a>
+//   </div>
+// </div>
