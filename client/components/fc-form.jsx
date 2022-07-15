78b5fb191b4df5d432403021975679e3a27adad8
Lance Cruzado <raphaellcruzado@gmail.com>
2022-07-15 05:36:38 +0000

    add flashcard

--- client/components/fc-form.jsx
+++ client/components/fc-form.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import {
   CDBBtn,
   CDBInput,
-  CDBCard
+  CDBCard,
+  CDBCardBody,
+  CDBCardTitle,
+  CDBCardText,
+  CDBContainer
 } from 'cdbreact';
 import { Col } from 'react-bootstrap/';
 // import * as FaIcons from 'react-icons/fa';
@@ -12,24 +16,45 @@ function FcForm({ cardData }) {
   return (
     cardData.map(card =>
       <>
-        <Col className='card-form-container'>
+        <CDBContainer className='card-form-container'>
           <Col className='card-form-card'>
-            <CDBBtn id='study-card' style={{ position: 'relative', left: '2.5rem' }} circle>
+            {/* <CDBBtn id='study-card' style={{ position: 'relative', left: '2.5rem' }} circle>
               <h4>Study Deck</h4>
-            </CDBBtn>
+            </CDBBtn> */}
             <form>
               <CDBInput type='text' placeholder='Front Content' style={{ width: '40rem' }} />
               <CDBInput type='text' placeholder='Back Content' style={{ width: '40rem' }} />
-              <CDBBtn id='study-card' style={{ position: 'relative', top: '.5rem', left: '2.5rem' }} circle>
+              <CDBBtn id='study-card' style={{ position: 'relative', top: '.5rem', left: '10rem' }} circle>
                 <h4>Create Card</h4>
               </CDBBtn>
             </form>
           </Col>
-        </Col>
+        </CDBContainer>
+        <CDBContainer className='card-form-container'>
+          <Col id='card-list'>
+          <>
+            <ul>
+              <li key={card.cardId}>
+                <CDBCard style={{ width: '25rem' }}>
+                  <CDBCardBody>
+                    <h5>{card.frontContent}</h5>
+                  </CDBCardBody>
+                </CDBCard>
+              </li>
+            </ul>
+          </>
+          </Col>
+        </CDBContainer>
       </>
     )
-
   );
 }
 
 export default FcForm;
+
+// {/* <CDBCard style={{ width: '25rem' }}>
+//   <CDBCardBody>
+//     <CDBCardTitle>Heading</CDBCardTitle>
+//     <CDBCardText>Paragraph</CDBCardText>
+//   </CDBCardBody>
+// </CDBCard> */}
