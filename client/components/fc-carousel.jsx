495d940becdf449befc6a8dbd4e8e7132e1d5407
Lance Cruzado <raphaellcruzado@gmail.com>
2022-07-26 05:57:45 +0000

    fc-carousel

--- client/components/fc-carousel.jsx
+++ client/components/fc-carousel.jsx
@@ -1,16 +1,14 @@
 import React, { useState } from 'react';
 
-export default function FlashCardCarousel() {
+export default function FlashCardCarousel({ cardData }) {
   const [flip, setFlip] = useState(false);
+  console.log(cardData);
   return (
-    <div className="card" style={{ width: '18rem' }}>
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card&apos;s content.</p>
-        <a href="#" className="card-link">Card link</a>
-        <a href="#" className="card-link">Another link</a>
-      </div>
+    <div
+    className={`card ${flip ? 'flip' : ''}`}
+    onClick={() => setFlip(!flip)}
+    >
+
     </div>
   );
 }
