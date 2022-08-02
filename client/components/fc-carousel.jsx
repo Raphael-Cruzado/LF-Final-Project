5a9ca162cce3b271f427357f6eb5424764c024a9
Lance Cruzado <raphaellcruzado@gmail.com>
2022-08-02 19:01:05 +0000

    flipping cards

--- client/components/fc-carousel.jsx
+++ client/components/fc-carousel.jsx
@@ -1,25 +1,30 @@
 import React, { useState } from 'react';
 import { CDBContainer } from 'cdbreact';
+import Card from 'react-bootstrap/Card';
 
 export default function FlashCardCarousel({ cardData }) {
   const [flip, setFlip] = useState(false);
   console.log(cardData);
   return (
     cardData.map(card =>
-      <>
+    <>
       <div className='card-container'>
-          <div
-            className={`card ${flip ? 'flip' : ''}`}
-            onClick={() => setFlip(!flip)}
-          >
-            <div className='front'>{card.frontContent}</div>
-            <div className='back'>{card.backContent}</div>
-            {flip ? card.backContent : card.frontContent}
-          </div>
+        <div className='card'></div>
       </div>
-
-      </>
+    </>
     )
-
   );
 }
+
+// {/* <>
+//   <div className='card-container'>
+//     <div
+//       className={`card ${flip ? 'flip' : ''}`}
+//       onClick={() => setFlip(!flip)}
+//     >
+//       <div className='front'>{card.frontContent}</div>
+//       <div className='back'>{card.backContent}</div>
+//       {flip ? card.backContent : card.frontContent}
+//     </div>
+//   </div>
+// </> */}
